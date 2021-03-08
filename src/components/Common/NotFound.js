be76257adb618b716c3602e7d90f694f46e27d01
zefe <zefetorres@icloud.com>
2021-03-08 10:14:53 -0600

    modified styles notfound

--- src/components/Common/NotFound.js
+++ src/components/Common/NotFound.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 export const NotFound = ({errorMsg}) => {
     return (
-        <div className="notfound">
-            <h3>{errorMsg}</h3>
+        <div className="notfound-container">
+            <h2>{errorMsg}</h2>
         </div>
     )
 }
